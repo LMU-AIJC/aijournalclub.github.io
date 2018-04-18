672167fe0692e48fe4fcffd8a9a4c5dfe6391ff8
Changkun Ou <hi@changkun.us>
2018-04-18 08:57:17 +0200

    update css styles

---
+++ schedule.js
@@ -0,0 +1,134 @@
+const schedule = [
+  {
+    "person": "Georgia Olympia Brikis",
+    "email": null,
+    "paper": "Dynamic Routing Between Capsules",
+    "link": "https://arxiv.org/abs/1710.09829",
+    "date": "18.12.2017",
+    "materials": [],
+    "tags": []
+  },
+  {
+    "person": "Sebastian Wagner",
+    "email": null,
+    "paper": "Learning Transferable Architectures for Scalable Image Recognition",
+    "link": "https://arxiv.org/abs/1707.07012",
+    "date": "08.01.2018",
+    "materials": [],
+    "tags": []
+  },
+  {
+    "person": "Changkun Ou",
+    "email": null,
+    "paper": "Understanding Deep Learning Requires Rethinking Generalization",
+    "link": "https://arxiv.org/abs/1611.03530",
+    "date": "15.01.2018",
+    "materials": [],
+    "tags": []
+  },
+  {
+    "person": "Azada Rustimova",
+    "email": null,
+    "paper": "Unsupervised Domain Adaptation with Similarity Learning",
+    "link": "https://arxiv.org/pdf/1711.08995.pdf",
+    "date": "22.01.2018",
+    "materials": [],
+    "tags": []
+  },
+  {
+    "person": "Changkun Ou",
+    "email": null,
+    "paper": "Generalization in Deep Learning",
+    "link": "https://arxiv.org/abs/1710.05468",
+    "date": "29.01.2018",
+    "materials": [
+      "https://docs.google.com/presentation/d/1eNUYH9_3bbBO7bONPU8BCTr2DOMC69gdOJvBgpLjwNg/edit?usp=drive_web&ouid=116465666322408862848"
+    ],
+    "tags": []
+  },
+  {
+    "person": "Sebastian Wagner",
+    "email": null,
+    "paper": "Special issue: Workshop on Reinforcement Learning",
+    "link": null,
+    "date": "05.02.2018",
+    "materials": [],
+    "tags": []
+  },
+  {
+    "person": "Georgia Olympia Brikis",
+    "email": null,
+    "paper": "One Model To Learn Them All",
+    "link": "https://arxiv.org/abs/1706.05137",
+    "date": "19.02.2018",
+    "materials": [],
+    "tags": []
+  },
+  {
+    "person": "Sebastian Wagner",
+    "email": null,
+    "paper": "A Deep Reinforced Model for Abstractive Summarization",
+    "link": "https://arxiv.org/abs/1705.04304",
+    "date": "26.02.2018",
+    "materials": [],
+    "tags": []
+  },
+  {
+    "person": "Azada Rustamova",
+    "email": null,
+    "paper": "A causal framework for explaining the predictions of black-box sequence-to-sequence models",
+    "link": "https://arxiv.org/abs/1707.01943",
+    "date": "05.03.2018",
+    "materials": [],
+    "tags": []
+  },
+  {
+    "person": "Changkun Ou",
+    "email": null,
+    "paper": "Matrix capsules with EM routing",
+    "link": "https://openreview.net/pdf?id=HJWLfGWRb",
+    "date": "12.03.2018",
+    "materials": [
+      "https://docs.google.com/presentation/d/1ccEsDtqjyQVvEvF-MDfwwnXqht5mPRE3c800vs5yJNY/edit?usp=drive_web&ouid=116465666322408862848"
+    ],
+    "tags": []
+  },
+  {
+    "person": "Georgia Olympia Brikis",
+    "email": null,
+    "paper": "Model-Agnostic Meta-Learning for Fast Adaption of Deep Networks",
+    "link": "https://arxiv.org/abs/1703.03400",
+    "date": "19.03.2018",
+    "materials": [],
+    "tags": []
+  },
+  {
+    "person": "Changkun Ou",
+    "email": null,
+    "paper": "Generalization in Machine Learning via Analytical Learning Theory",
+    "link": "https://arxiv.org/abs/1802.07426",
+    "date": "09.04.2018",
+    "materials": [],
+    "tags": []
+  },
+  {
+    "person": "Zhenyu Chen",
+    "email": null,
+    "paper": "A Review on Deep Learning Techniques Applied to Semantic Segmentation",
+    "link": null,
+    "date": "16.04.2018",
+    "materials": [
+      "https://docs.google.com/presentation/d/1BULnfmv4FlNSmMHYEpSUWIp3GAInDSCTn4YVvkPIO78/edit?usp=sharing"
+    ],
+    "tags": []
+  },
+  {
+    "person": "Azada Rustamova",
+    "email": null,
+    "paper": "Position-aware Attention and Supervised Data Improve Slot Filling",
+    "link": "https://nlp.stanford.edu/pubs/zhang2017tacred.pdf",
+    "date": "23.04.2018",
+    "materials": [],
+    "tags": []
+  }
+]
